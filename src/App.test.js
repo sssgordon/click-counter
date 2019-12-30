f6ec0395a782b3e4d8389dd8b827392c0af6619e
sssgordon <gso7@uwo.ca>
2019-12-31 00:42:17 +0100

    Eject

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+// test anything CRITICAL to the app
+
+test("renders without error", () => {
+    const wrapper = shallow(<App />);
+    const appComponent = wrapper.find("[data-test='component-app']");
+    expect(appComponent.length).toBe(1);
 });
+
+test("renders increment button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter starts at 0", () => {});
+
+test("clicking button increments counter display", () => {});
