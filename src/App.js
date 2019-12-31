65ff2342fd8a714ee9255b960f337d2821ec5b02
sssgordon <gso7@uwo.ca>
2019-12-31 01:46:37 +0100

    Fully tested counter app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+    state = {
+        counter: 0
+    };
     render() {
-        return <div data-test="component-app"></div>;
+        return (
+            <div data-test="component-app">
+                <h1 data-test="counter-display">
+                    The counter is currently {this.state.counter}
+                </h1>
+                <button
+                    data-test="increment-button"
+                    onClick={() =>
+                        this.setState({ counter: this.state.counter + 1 })
+                    }
+                >
+                    Increment counter
+                </button>
+            </div>
+        );
     }
 }
 
